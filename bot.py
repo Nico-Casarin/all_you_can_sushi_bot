3666fb59c86607919b288f7e9cb6e741941796e0
Nico-Casarin <nico.casarin@gmail.com>
2025-02-11 00:11:03 +0100

    Main logic implementation. Functioning app

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def init_db(db):
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS session (
             id TEXT PRIMARY KEY,
-            attiva INTEGER DEFAULT 1
+            active INTEGER DEFAULT 1
         )
     """)
 
@@ -33,12 +33,14 @@ def init_db(db):
         CREATE TABLE IF NOT EXISTS orders (
             session_id TEXT,
             user TEXT,
-            piatto INTEGER,
-            quantita INTEGER,
-            PRIMARY KEY (session_id, user, piatto),
-            FOREIGN KEY (session_id) REFERENCES sessioni(id)
+            plate INTEGER,
+            quantity INTEGER,
+            PRIMARY KEY (session_id, user, plate),
+            FOREIGN KEY (session_id) REFERENCES session(id)
         )
     """)
+
+    print('test')
 
     conn.commit()
     conn.close()
@@ -46,11 +48,80 @@ def init_db(db):
 async def new_session(update: Update, context: ContextTypes.DEFAULT_TYPE):
     session_id = str(random.randint(10000, 99999))
 
-    conn = sqlite3.connect("ordini.db")
+    conn = sqlite3.connect(db)
     cursor = conn.cursor()
     cursor.execute("INSERT INTO session (id) VALUES (?)", (session_id,))
     conn.commit()
     conn.close()
+
+    await update.message.reply_text(f"New session created! Session ID: {session_id}")
+
+async def new_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text.split()
+
+    if len(msg) != 2 or not msg[0].isdigit() or not msg[1].isdigit():
+        await update.message.reply_text("Wrong fromat! Use: 'plate_number quantity'.")
+        return
+
+    plate, quantity = map(int, msg)
+    user = update.message.from_user.username
+
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM session WHERE active = 1 ORDER BY id DESC LIMIT 1")
+    session = cursor.fetchone()
+    if not session:
+        await update.message.reply_text("No active sessione. Wait for a new session")
+        conn.close()
+        return
+
+    session_id = session[0]
+    cursor.execute("""
+        INSERT INTO orders (session_id, user, plate, quantity)
+        VALUES (?, ?, ?, ?)
+        ON CONFLICT(session_id, user, plate) DO UPDATE SET quantity = quantity + ?
+    """, (session_id, user, plate, quantity, quantity))
+
+    conn.commit()
+    conn.close()
+
+    await update.message.reply_text(f"Order saved for plate n. {plate}")
+
+async def close_session(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM session WHERE active = 1 ORDER BY id DESC LIMIT 1")
+    session = cursor.fetchone()
+
+    if not session:
+        await update.message.reply_text("No Active session")
+        conn.close()
+        return
+
+    session_id = session[0]
+    cursor.execute("UPDATE session SET active = 0 WHERE id = ?", (session_id,))
+
+    cursor.execute("""
+        SELECT plate, SUM(quantity) FROM orders
+        WHERE session_id = ?
+        GROUP BY plate
+    """, (session_id,))
+
+    consolidated_orders = cursor.fetchall()
+    conn.commit()
+    conn.close()
+
+    if not consolidated_orders:
+        await update.message.reply_text("No orders found!")
+        return
+
+    message = f" **Orders summary for session {session_id}:**\n"
+    for plate, quantity in consolidated_orders:
+        message += f"- Plate {plate}: {quantity} times\n"
+
+    await update.message.reply_text(message)
 
 def main():
 
@@ -63,14 +134,17 @@ def main():
 
     api_token = args.token
 
-    global order_db
-    order_db = args.order
+    global db
+    db = args.order
 
-    init_db(order_db)
+    init_db(db)
 
     application = ApplicationBuilder().token(api_token).build()
-   # hanlder =  TypeHandler(Update, callback)
-   # application.add_handler(handler, -1)
+    # hanlder =  TypeHandler(Update, callback)
+    # application.add_handler(handler, -1)
+    application.add_handler(CommandHandler("new_session", new_session))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, new_order))
+    application.add_handler(CommandHandler("close_session", close_session))
 
 
     application.run_polling()
