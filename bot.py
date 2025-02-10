45b87c9cf7fbc4ee8407c9db9d587fa7360410e5
Nico-Casarin <nico.casarin@gmail.com>
2025-02-10 22:56:12 +0100

    initialization of bot db creation

--- bot.py
+++ bot.py
@@ -1,3 +1,78 @@
-import 
+import logging
 import sqlite3
+import argparse
+import random
+from telegram import Update
+
+from telegram.ext import (
+    ApplicationBuilder, ContextTypes, CommandHandler, filters,
+    MessageHandler, TypeHandler , ApplicationHandlerStop,
+    JobQueue)
+
+import os
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+# Inizializza il database SQLite
+def init_db():
+    conn = sqlite3.connect("ordini.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS session (
+            id TEXT PRIMARY KEY,
+            attiva INTEGER DEFAULT 1
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS orders (
+            session_id TEXT,
+            user TEXT,
+            piatto INTEGER,
+            quantita INTEGER,
+            PRIMARY KEY (session_id, user, piatto),
+            FOREIGN KEY (session_id) REFERENCES sessioni(id)
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+async def new_session(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    session_id = str(random.randint(10000, 99999))
+
+    conn = sqlite3.connect("ordini.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO session (id) VALUES (?)", (session_id,))
+    conn.commit()
+    conn.close()
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-t', '--token', help = 'Bot Token', required = True)
+    parser.add_argument('-o', '--order', help = 'Order DB', required = True)
+
+    args = parser.parse_args()
+
+    api_token = args.token
+
+    global order_db
+    order_db = args.order
+
+    application = ApplicationBuilder().token(api_token).build()
+    hanlder =  TypeHandler(Update, Callback)
+    application.add_handler(handler, -1)
+
+
+    application.run_polling()
+
+if __name__=='__main__':
+    main()
 
